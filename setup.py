743b9ba3bb959f3a55e831a56604e91c07163d47
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-07-02 13:34:05 +0200

    chore(reqs): requirements setup
    - Add requirements.txt
    - Add setup.py
    - Add __init__.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mana",
+    version="0.0.1",
+    author="IISY at Beuth",
+    author_email="iisy@beuth-hochschule.de",
+    description="A Motion ANAlysis toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.beuth-hochschule.de/iisy/mana",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
