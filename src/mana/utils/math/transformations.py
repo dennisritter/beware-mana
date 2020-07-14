16ed831c924ede38e317057ecfaa4fd532ac33bf
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-07-14 16:07:57 +0200

    fix(transformations): Fix dot and get_angle
    -Add missing assert statemenst in tests...
    -Change expected test results to radians instead of degrees
    -remove implicit normalization in dot function
    -handle rounding error for dot product of same normalized vector

--- src/mana/utils/math/transformations.py
+++ src/mana/utils/math/transformations.py
@@ -6,8 +6,8 @@ import sklearn.preprocessing as preprocessing
 def norm(v: 'np.ndarray') -> 'np.ndarray':
     """Normalizes the given vector or array of vectors vec and returns it. Returns a numpy array that either represents the normalized vector or an array of normalized vectors.
 
-        Args:
-            vec_arr (np.ndarray): Either a 1-D array that represents the vector to normalize or a 2-D array containing the vectors to normalize.
+    Args:
+        vec_arr (np.ndarray): Either a 1-D array that represents the vector to normalize or a 2-D array containing the vectors to normalize.
     """
     if type(v) != np.ndarray:
         raise ValueError('v must be of type numpy.ndarray')
@@ -26,28 +26,29 @@ def norm(v: 'np.ndarray') -> 'np.ndarray':
 
 
 def get_angle(v1, v2):
-    """Returns the angle (degrees) between the vectors v1, v2 if single vectors are given (v1,v2 ndim == 1).
-       Returns an array of angles (degrees) between respective vectors in v1, v2 arrays of vectors ((v1,v2 ndim == 2)).
+    """Returns the angle (radians) between the vectors v1, v2 if single vectors are given (v1,v2 ndim == 1).
+       Returns an array of angles (radians) between respective vectors in v1, v2 arrays of vectors ((v1,v2 ndim == 2)).
 
-        Args:
-            v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
-            v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+    Args:
+        v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+        v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
     """
     if type(v1) != np.ndarray or type(v2) != np.ndarray:
         raise ValueError('v1 and v2 must both be of type numpy.ndarray')
     elif v1.ndim != v2.ndim or v1.ndim > 2:
         raise ValueError('v1 and v2 must both be either 1- or 2-dimensional')
-
-    return np.arccos(dot(norm(v1), norm(v2)))
+    # * The dot product of two equal vectors will result in a minor rounding error (dot(norm([1,1,1])) = dot([0.57735027, 0.57735027, 0.57735027]) = 1.0000000000000002)
+    # * As np.arccos can only handle values from 0.0 to 1.0 (inclusive), we clamp the result to this range, too.
+    return np.arccos(np.clip(dot(norm(v1), norm(v2)), 0.0, 1.0))
 
 
 def dot(v1, v2):
     """Returns the dot product between v1 and v2 (v1,v2 ndim == 1).
        Returns an array of dot products between respective vectors in v1 and v2 (v1,v2 ndim == 2).
 
-        Args:
-            v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
-            v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+    Args:
+        v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+        v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
     """
     if type(v1) != np.ndarray or type(v2) != np.ndarray:
         raise ValueError('v1 and v2 must both be of type numpy.ndarray')
@@ -55,6 +56,23 @@ def dot(v1, v2):
         raise ValueError('v1 and v2 must both be either 1- or 2-dimensional')
 
     if v1.ndim == 1 and v2.ndim == 1:
-        return np.dot(norm(v1), norm(v2))
+        return np.dot(v1, v2)
     elif v1.ndim == 2 and v2.ndim == 2:
         return (v1 @ v2.transpose()).diagonal()
+
+
+def v3_to_v4(v):
+    """Returns a 3-D position vector with appended homogenious coordinate from the given 3-D vector or an array of such vectors.
+
+    Args:
+        v (np.ndarray): A 3-D vector or an array of 3-D vectors.
+    """
+    if type(v) != np.ndarray or type(v) != np.ndarray:
+        raise ValueError('v must be of type numpy.ndarray')
+    elif v.ndim > 2:
+        raise ValueError('v must be either 1- or 2-dimensional (v.ndim == 1|2)')
+
+    if v.ndim == 1:
+        return np.append(v, 1)
+    elif v.ndim == 2:
+        return np.hstack((v, np.ones(len(v)).reshape((len(v), 1))))
--- tests/utils/math/test_transformations.py
+++ tests/utils/math/test_transformations.py
@@ -34,27 +34,43 @@ def test_norm_batch():
 
 
 # get_angle
-@pytest.mark.parametrize('v1, v2, expected', [(np.array([1, 1, 1]), np.array([1, 1, 1]), 0.0), (np.array([1, 0, 0]), np.array([0, 1, 0]), 90.0),
-                                              (np.array([1, 0, 0]), np.array([1, 1, 0]), 45.0)])
-def test_get_angle_single(v1, v2, expected):
-    t.get_angle(v1, v2) == expected
-
-
-@pytest.mark.parametrize('v1, v2, expected',
-                         [(np.array([[1, 1, 1], [1, 0, 0], [1, 0, 0]]), np.array([[1, 1, 1], [0, 1, 0], [1, 1, 0]]), np.array([0.0, 90.0, 45.0]))])
-def test_get_angle_batch(v1, v2, expected):
-    np.array_equal(t.get_angle(v1, v2), expected)
-
-
-# dot
 @pytest.mark.parametrize('v1, v2, expected', [(np.array([1, 1, 1]), np.array([1, 1, 1]), 0.0), (np.array([1, 0, 0]), np.array([0, 1, 0]), math.pi / 2),
                                               (np.array([1, 0, 0]), np.array([1, 1, 0]), math.pi / 4)])
-def test_dot_single(v1, v2, expected):
-    t.dot(v1, v2) == expected
+def test_get_angle_single(v1, v2, expected):
+    assert t.get_angle(v1, v2) == pytest.approx(expected)
 
 
 @pytest.mark.parametrize('v1, v2, expected',
                          [(np.array([[1, 1, 1], [1, 0, 0], [1, 0, 0]]), np.array([[1, 1, 1], [0, 1, 0], [1, 1, 0]]), np.array([0.0, math.pi / 2, math.pi / 4]))]
                          )
+def test_get_angle_batch(v1, v2, expected):
+    result = t.get_angle(v1, v2)
+    # Can't assert with pyterst.approx in array of vectors
+    assert result[0] == pytest.approx(expected[0])
+    assert result[1] == pytest.approx(expected[1])
+    assert result[2] == pytest.approx(expected[2])
+
+
+# dot
+@pytest.mark.parametrize('v1, v2, expected', [(np.array([1, 1, 1]), np.array([1, 1, 1]), math.cos(0.0)),
+                                              (np.array([1, 0, 0]), np.array([0, 1, 0]), math.cos(math.pi / 2)),
+                                              (np.array([1, 0, 0]), np.array([1, 1, 0]), math.cos(math.pi / 4))])
+def test_dot_single(v1, v2, expected):
+    assert t.dot(t.norm(v1), t.norm(v2)) == pytest.approx(expected)
+
+
+@pytest.mark.parametrize('v1, v2, expected', [(np.array([[1, 1, 1], [1, 0, 0], [1, 0, 0]]), np.array(
+    [[1, 1, 1], [0, 1, 0], [1, 1, 0]]), np.array([math.cos(0.0), math.cos(math.pi / 2), math.cos(math.pi / 4)]))])
 def test_dot_batch(v1, v2, expected):
-    np.array_equal(t.dot(v1, v2), expected)
+    # Can't assert with pyterst.approx in array of vectors
+    result = t.dot(t.norm(v1), t.norm(v2))
+    assert result[0] == pytest.approx(expected[0])
+    assert result[1] == pytest.approx(expected[1])
+    assert result[2] == pytest.approx(expected[2])
+
+
+# v3_to_v4
+@pytest.mark.parametrize('v, expected', [(np.array([1, 1, 1]), 4)])
+def test_v3_to_v4_single(v, expected):
+    result = len(t.v3_to_v4(v))
+    assert result == pytest.approx(expected)
