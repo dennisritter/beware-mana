2807a21c9069430e187c44b2e12907478eb17860
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-07-14 14:18:13 +0200

    feat(transformations): Add dot + get_angle
    -Add dot function
    -Add get_angle function
    -Add tests
    -Add Exceptions for wrong parameters
    -Add docs

--- src/mana/utils/math/transformations.py
+++ src/mana/utils/math/transformations.py
@@ -3,18 +3,58 @@ import numpy as np
 import sklearn.preprocessing as preprocessing
 
 
-def norm(vec_arr: 'np.ndarray') -> 'np.ndarray':
+def norm(v: 'np.ndarray') -> 'np.ndarray':
     """Normalizes the given vector or array of vectors vec and returns it. Returns a numpy array that either represents the normalized vector or an array of normalized vectors.
 
-    Args:
-        vec_arr (np.ndarray): Either a 1-D array that represents the vector to normalize or a 2-D array containing the vectors to normalize.
+        Args:
+            vec_arr (np.ndarray): Either a 1-D array that represents the vector to normalize or a 2-D array containing the vectors to normalize.
     """
-    if vec_arr.ndim == 1:
-        vec_norm = np.linalg.norm(vec_arr)
+    if type(v) != np.ndarray:
+        raise ValueError('v must be of type numpy.ndarray')
+    elif v.ndim > 2:
+        raise ValueError('v must be either 1- or 2-dimensional')
+
+    if v.ndim == 1:
+        vec_norm = np.linalg.norm(v)
 
         # No zero division
         if vec_norm == 0:
-            return np.zeros(len(vec_arr))
-        return vec_arr / vec_norm
-    if vec_arr.ndim == 2:
-        return preprocessing.normalize(vec_arr, norm='l2')
+            return np.zeros(len(v))
+        return v / vec_norm
+    if v.ndim == 2:
+        return preprocessing.normalize(v, norm='l2')
+
+
+def get_angle(v1, v2):
+    """Returns the angle (degrees) between the vectors v1, v2 if single vectors are given (v1,v2 ndim == 1).
+       Returns an array of angles (degrees) between respective vectors in v1, v2 arrays of vectors ((v1,v2 ndim == 2)).
+
+        Args:
+            v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+            v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+    """
+    if type(v1) != np.ndarray or type(v2) != np.ndarray:
+        raise ValueError('v1 and v2 must both be of type numpy.ndarray')
+    elif v1.ndim != v2.ndim or v1.ndim > 2:
+        raise ValueError('v1 and v2 must both be either 1- or 2-dimensional')
+
+    return np.arccos(dot(norm(v1), norm(v2)))
+
+
+def dot(v1, v2):
+    """Returns the dot product between v1 and v2 (v1,v2 ndim == 1).
+       Returns an array of dot products between respective vectors in v1 and v2 (v1,v2 ndim == 2).
+
+        Args:
+            v1 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+            v2 (np.ndarray): A 3-D vector or an array of 3-D vectors.
+    """
+    if type(v1) != np.ndarray or type(v2) != np.ndarray:
+        raise ValueError('v1 and v2 must both be of type numpy.ndarray')
+    elif v1.ndim != v2.ndim or v1.ndim > 2:
+        raise ValueError('v1 and v2 must both be either 1- or 2-dimensional')
+
+    if v1.ndim == 1 and v2.ndim == 1:
+        return np.dot(norm(v1), norm(v2))
+    elif v1.ndim == 2 and v2.ndim == 2:
+        return (v1 @ v2.transpose()).diagonal()
--- tests/utils/math/test_transformations.py
+++ tests/utils/math/test_transformations.py
@@ -1,8 +1,10 @@
 import pytest
 import mana.utils.math.transformations as t
 import numpy as np
+import math
 
 
+# norm
 def test_norm_zero_vec():
     zero_vector = np.zeros(3)
     result = t.norm(zero_vector)
@@ -29,3 +31,30 @@ def test_norm_vec():
 def test_norm_batch():
     result = t.norm(np.array([[0, 0, 0], [1, 0, 0], [3, 4, 0]]))
     assert np.array_equal(result, np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.6, 0.8, 0.0]]))
+
+
+# get_angle
+@pytest.mark.parametrize('v1, v2, expected', [(np.array([1, 1, 1]), np.array([1, 1, 1]), 0.0), (np.array([1, 0, 0]), np.array([0, 1, 0]), 90.0),
+                                              (np.array([1, 0, 0]), np.array([1, 1, 0]), 45.0)])
+def test_get_angle_single(v1, v2, expected):
+    t.get_angle(v1, v2) == expected
+
+
+@pytest.mark.parametrize('v1, v2, expected',
+                         [(np.array([[1, 1, 1], [1, 0, 0], [1, 0, 0]]), np.array([[1, 1, 1], [0, 1, 0], [1, 1, 0]]), np.array([0.0, 90.0, 45.0]))])
+def test_get_angle_batch(v1, v2, expected):
+    np.array_equal(t.get_angle(v1, v2), expected)
+
+
+# dot
+@pytest.mark.parametrize('v1, v2, expected', [(np.array([1, 1, 1]), np.array([1, 1, 1]), 0.0), (np.array([1, 0, 0]), np.array([0, 1, 0]), math.pi / 2),
+                                              (np.array([1, 0, 0]), np.array([1, 1, 0]), math.pi / 4)])
+def test_dot_single(v1, v2, expected):
+    t.dot(v1, v2) == expected
+
+
+@pytest.mark.parametrize('v1, v2, expected',
+                         [(np.array([[1, 1, 1], [1, 0, 0], [1, 0, 0]]), np.array([[1, 1, 1], [0, 1, 0], [1, 1, 0]]), np.array([0.0, math.pi / 2, math.pi / 4]))]
+                         )
+def test_dot_batch(v1, v2, expected):
+    np.array_equal(t.dot(v1, v2), expected)
