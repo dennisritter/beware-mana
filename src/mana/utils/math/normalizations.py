c746ac460a3369725268f520a8a61e5d77838b23
Christopher <christopher.kuemmel@beuth-hochschule.de>
2020-07-17 15:13:16 +0200

    feat(normalizations): all poses mean
    - Implement all poses mean subtraction normalization
    - Add test case

--- src/mana/utils/math/normalizations.py
+++ src/mana/utils/math/normalizations.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def first_pose_mean(array: np.ndarray) -> np.ndarray:
     """Computes the mean of the first frame and subtracts this from all frames.
+
     The mean will be computed for each axis.
 
     Args:
@@ -21,3 +22,30 @@ def first_pose_mean(array: np.ndarray) -> np.ndarray:
     return array - np.mean(array[mean_slice], axis=0)
 
 
+def all_poses_mean(array, mode='all'):
+    """Computes the mean of the frame(s) and subtracts it.
+
+    There are two modes (single|all) where in *single* the mean will be
+    computed for each frame and subtracted for each frame seperately.
+    The mode *all* will compute a combined mean over all frames and
+    subtract it for all frames respectively.
+    The mean will be computed for each axis.
+
+    Args:
+        array (np.ndarray): The input array of abritary dimensionality >=2.
+            Where the first dimension = number of frames,
+            the second lst dimension = number of positions,
+            and the last dimension = number of axis.
+        mode (str): The mode on how to compute the mean (single|all). 
+            Defaults to *all*.
+
+    Returns:
+        np.ndarray: The array subtracted by the mean of the first frame.
+    """
+    if array.ndim < 3:
+        raise ValueError('Array need to have at least three dimensions!')
+
+    if mode == 'single':
+        return array - np.expand_dims(np.mean(array, axis=-2), -2)
+    else:
+        return array - np.mean(array, axis=(0, -2))
