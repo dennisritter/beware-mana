769879b8ded5c7c7d8459cbc9082bd36c8d4e34d
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-09-16 11:16:00 +0200

    feat(sequence transform): body part mapping
    -Add callable transformation class to morph mka to iisy bp model

--- src/mana/utils/math/sequence_transform.py
+++ src/mana/utils/math/sequence_transform.py
@@ -101,6 +101,98 @@ class FlipZ:
         return positions
 
 
+class mka_to_iisy_bodyparts:
+    """Callable transform class to flip z-values."""
+    def __call__(self, positions: np.ndarray) -> np.ndarray:
+        """Transforms MKA recorded motion data into the iisy body part format.
+
+        Will Transform MKA human motion model (32 body parts) into the iisy
+        body part model (16 body parts).
+        
+        # * MKA body part model (ORIGIN)
+        # body_parts_mka = {
+        #     "Pelvis": 0,
+        #     "SpineNavel": 1,
+        #     "SpineChest ": 2,
+        #     "Neck": 3,
+        #     "ClavicleLeft": 4,
+        #     "ShoulderLeft": 5,
+        #     "ElbowLeft ": 6,
+        #     "WristLeft ": 7,
+        #     "HandLeft ": 8,
+        #     "HandTipLeft": 9,
+        #     "ThumbLeft": 10,
+        #     "ClavicleRight": 11,
+        #     "ShoulderRight": 12,
+        #     "ElbowRight": 13,
+        #     "WristRight": 14,
+        #     "HandRight": 15,
+        #     "HandTipRight": 16,
+        #     "ThumbRight": 17,
+        #     "HipLeft": 18,
+        #     "KneeLeft": 19,
+        #     "AnkleLeft": 20,
+        #     "FootLeft": 21,
+        #     "HipRight": 22,
+        #     "KneeRight": 23,
+        #     "AnkleRight": 24,
+        #     "FootRight": 25,
+        #     "Head": 26,
+        #     "Nose": 27,
+        #     "EyeLeft": 28,
+        #     "EarLeft": 29,
+        #     "EyeRight": 30,
+        #     "EarRight": 31
+        # }
+
+        # * IISY body part model (TARGET)
+        # body_parts_iisy = {
+        #     "head": 0,
+        #     "neck": 1,
+        #     "shoulder_l": 2,
+        #     "shoulder_r": 3,
+        #     "elbow_l": 4,
+        #     "elbow_r": 5,
+        #     "wrist_l": 6,
+        #     "wrist_r": 7,
+        #     "torso": 8,
+        #     "pelvis": 9,
+        #     "hip_l": 10,
+        #     "hip_r": 11,
+        #     "knee_l": 12,
+        #     "knee_r": 13,
+        #     "ankle_l": 14,
+        #     "ankle_r": 15,
+        # }
+
+        Args:
+            positions (np.ndarray): A time series of various 3-D positions
+            (ndim = 3) (shape = (n_frames, n_positions, 3))
+
+        Returns:
+            np.ndarray: The transformed positions array.
+        """
+        transformed_positions = np.zeros((len(positions), 16, 3))
+        transformed_positions[:, 0, :] = positions[:, 26, :]
+        transformed_positions[:, 1, :] = positions[:, 3, :]
+        transformed_positions[:, 2, :] = positions[:, 5, :]
+        transformed_positions[:, 3, :] = positions[:, 12, :]
+        transformed_positions[:, 4, :] = positions[:, 6, :]
+        transformed_positions[:, 5, :] = positions[:, 13, :]
+        transformed_positions[:, 6, :] = positions[:, 8, :]
+        transformed_positions[:, 7, :] = positions[:, 14, :]
+        transformed_positions[:, 8, :] = positions[:, 1, :]
+        transformed_positions[:, 9, :] = positions[:, 0, :]
+        transformed_positions[:, 10, :] = positions[:, 18, :]
+        transformed_positions[:, 11, :] = positions[:, 22, :]
+        transformed_positions[:, 12, :] = positions[:, 19, :]
+        transformed_positions[:, 13, :] = positions[:, 23, :]
+        transformed_positions[:, 14, :] = positions[:, 20, :]
+        transformed_positions[:, 15, :] = positions[:, 24, :]
+
+        return transformed_positions
+
+
 ## Just a template for a transform with basic memory
 # class some_transform_that_needs_memory:
 #     def __init__(self, some_memorized_param):
