6c876ba5548d5c19b00ee7298a94375c8282adee
Christopher <christopher.kuemmel@beuth-hochschule.de>
2020-10-30 16:21:10 +0100

    ref(normalizations): pose orientation
    - Complete update of pose orientation method

--- src/mana/utils/math/normalizations.py
+++ src/mana/utils/math/normalizations.py
@@ -72,99 +72,117 @@ def pose_position(array: np.ndarray, position: np.ndarray) -> np.ndarray:
 
 
 def pose_orientation(array: np.ndarray,
-                     rotation_vectors: np.ndarray,
-                     plane_normals: np.ndarray,
-                     origins: np.ndarray = None) -> np.ndarray:
-    """Rotates all positions towards a plane that is defined by its normal
-    vector and returns the rotated positions.
-
-    The rotation will be computed based on the angle between the given
-    rotation vectors and the plane normals (or actually the plane itself) so
-    that each rotation vector is aligned to the defined plane. The resulting
-    Rotations will then be applied about the given origins.
+                     v_from: np.ndarray,
+                     v_to: np.ndarray,
+                     axis: np.ndarray = None,
+                     origin: np.ndarray = None) -> np.ndarray:
+    """Rotates all positions around an axis based on the angle computed between
+    v_from to v_to around axis, so that v_from is parallel to v_to.
 
     Args:
         array (np.ndarray): The input array of dimensionality =3.
             Where the first dimension = number of frames,
             the second dimension = number of positions,
             and the last dimension = number of axis.
-        rotation_vectors (np.ndarray): A point vector or an array of point
-            vectors with size equal to number of frames.
-        plane_normals (np.ndarray): A vector that defines the normal the plane
-            where the array should be rotated to. Can either contain a
-            3-d vector or an array of 3-d vectors with size = number of input
-            frames.
-        origins (np.ndarray): The point to rotate about. Can either
-            contain 3-d point coordinates or a list of 3-d point coordinates
-            with size = number of input frames.
+        v_from (np.ndarray): The input vector to start rotation from. Can
+            either be a 3d vector or an array of 3d vectors.
+        v_to (np.ndarray): The target vector to rotate to. Can either be a 3d
+            vector or an array of 3d vectors.
+        axis (np.ndarray): The rotation axis. Can either be a 3d vector or an
+            array of 3d vectors. Defaults to ([0, 0, 1]).
+        origin (np.ndarray): The point to rotate about. Can either
+            contain 3-d point coordinates or an array of 3-d point coordinates
+            with size = number of input frames. Defaults to ([0, 0, 0]).
+
+    Note: You can supply one or number of frames (array) distinct vectors for
+        each parameter. If one vector is suppled it will be broadcasted. In
+        case of number of frames vectors, those got applied to each frames
+        seperately.
 
     Returns:
-        np.ndarray: The array rotated to the plane.
+        np.ndarray: The rotated array around the axis.
 
     Raises:
-        ValueError: if input array is not of dimensionality = 3.
-        ValueError: if array of rotation, plane normals or origins contain more
-        than two dimensions (ndim > 2).
-        ValueError: if array of rotation, plane normals or origins do not
-        contain 3-D vectors/points or an array of 3-D vectors/points whose
-        length equals the number of frames in array (array.shape[0]).
-
+        ValueError: if array is not of dimensionality equals 3.
+        ValueError: if v_from is not of dimensionality equals 1 or 2.
+        ValueError: if v_to is not of dimensionality equals 1 or 2.
+        ValueError: if axis is not of dimensionality equals 1 or 2.
+        ValueError: if origin is not of dimensionality equals 1 or 2.
     """
-    if not isinstance(origins, np.ndarray):
-        if not origins:
-            origins = np.array([0, 0, 0])
+    if not isinstance(axis, np.ndarray):
+        if not axis:
+            axis = np.array([0, 0, 0])
+    if not isinstance(origin, np.ndarray):
+        if not origin:
+            origin = np.array([0, 0, 0])
 
     n_frames = array.shape[0]
 
     if array.ndim != 3:
         raise ValueError('Input array must be of dimensionality = 3!')
 
-    if rotation_vectors.ndim > 2:
-        raise ValueError('Rotation vectors must be a 1 or 2 dimensional numpy '
-                         'array')
-    if rotation_vectors.shape[0] not in [3, n_frames]:
-        raise ValueError('Rotation vectors must contain one vector or number '
+    if v_from.ndim > 2:
+        raise ValueError('v_from must be a 1 or 2 dimensional numpy array')
+    if v_from.shape[0] not in [3, n_frames]:
+        raise ValueError('v_from must contain one vector or number '
                          'of frames times two vectors!')
 
-    if plane_normals.ndim > 2:
-        raise ValueError(
-            'Plane normals must be a 1 or 2 dimensional numpy array')
-    if plane_normals.shape[0] not in [3, n_frames]:
-        raise ValueError('Plane normals must contain only one vector '
+    if v_to.ndim > 2:
+        raise ValueError('v_to must be a 1 or 2 dimensional numpy array')
+    if v_to.shape[0] not in [3, n_frames]:
+        raise ValueError('v_to must contain only one vector '
                          '(shape = 3) or as many vectors as frames.')
 
-    if origins.ndim > 2:
-        raise ValueError('Origins must be a 1 or 2 dimensional numpy array')
-    if origins.shape[0] not in [3, n_frames]:
-        raise ValueError('Origins must contain only one vector '
+    if axis.ndim > 2:
+        raise ValueError('axis must be a 1 or 2 dimensional numpy array')
+    if axis.shape[0] not in [3, n_frames]:
+        raise ValueError('axis must contain only one vector '
                          '(shape = 3) or as many vectors as frames.')
 
-    if rotation_vectors.ndim == 1:
-        rotation_vectors = np.expand_dims(rotation_vectors, 0)
-    if plane_normals.ndim == 1:
-        plane_normals = np.expand_dims(plane_normals, 0)
-    if origins.ndim == 1:
-        origins = np.expand_dims(origins, 0)
+    if origin.ndim > 2:
+        raise ValueError('Origin must be a 1 or 2 dimensional numpy array')
+    if origin.shape[0] not in [3, n_frames]:
+        raise ValueError('Origin must contain only one vector '
+                         '(shape = 3) or as many vectors as frames.')
 
-    # compute rotation angle based on rotation vectors and plane orthogonals
-    alphas = mt.angle_complementary(rotation_vectors, plane_normals)
+    if v_from.ndim == 1:
+        v_from = np.expand_dims(v_from, 0)
+    if v_to.ndim == 1:
+        v_to = np.expand_dims(v_to, 0)
+    if axis.ndim == 1:
+        axis = np.expand_dims(axis, 0)
+    if origin.ndim == 1:
+        origin = np.expand_dims(origin, (0, 1))
+    if origin.ndim == 2:
+        origin = np.expand_dims(origin, (1))
 
-    # If more than one rotation vector is given but only one plane normal is
-    # present, repeat to an array of (same) plane normals.
-    if plane_normals.shape[0] == 1 and rotation_vectors.shape[0] > 1:
-        plane_normals = np.full(rotation_vectors.shape, (plane_normals[0]))
+    if v_from.shape != v_to:
+        if v_from.shape[0] < v_to.shape[0]:
+            v_from = np.full(v_to.shape, (v_from[0]))
+        else:
+            v_to = np.full(v_from.shape, (v_to[0]))
 
-    # If more than one plane normal is given but only one rotation vector is
-    # present, repeat to an array of (same) rotation vectors.
-    if rotation_vectors.shape[0] == 1 and plane_normals.shape[0] > 1:
-        rotation_vectors = np.full(plane_normals.shape, (rotation_vectors[0]))
+    # subtract origin from positions
+    array = array - origin
 
-    # compute rotation axis from rotation vector and plane normals
-    axes = np.abs(mt.orthogonal_vector(rotation_vectors, plane_normals))
+    # TODO: remove repeat when orthogonal_vector method support broadcasting
+    # If more than one input vector is given but only one axis is present,
+    # repeat to an array of (same) axis.
+    if axis.shape[0] == 1 and v_from.shape[0] > 1:
+        axis = np.full(v_from.shape, (axis[0]))
 
-    # create rotation matrix about axis
-    rotations = mt.rotation(axes, alphas)
+    # compute cross product from vectors to rotation axis (plane normals)
+    v_from_cross_axis = mt.orthogonal_vector(v_from, axis)
+    v_to_cross_axis = mt.orthogonal_vector(v_to, axis)
+
+    # compute angle between cross products
+    alpha = mt.angle(v_from_cross_axis, v_to_cross_axis)
+
+    # rotation matrix
+    rotation = mt.rotation(axis, alpha)
 
     # apply rotation matrix to vector and return
     # therefore we need to transpose the input arrays
-    return np.transpose(rotations @ np.transpose(array, (0, 2, 1)), (0, 2, 1))
+    # + (re)add the origin vector
+    return np.transpose(rotation @ np.transpose(array, (0, 2, 1)),
+                        (0, 2, 1)) + origin
